12bf0e8d81f03caef19d0e0da83b52556fbed1aa
gus <gus@cf90e129-9db2-4be2-879b-6cf61410cf68>
2016-06-01 08:52:05 +0000

    git-svn-id: https://harpo.uv.es/repos/ipl/Software@653 cf90e129-9db2-4be2-879b-6cf61410cf68

--- simpleClass.m
+++ simpleClass.m
@@ -47,7 +47,7 @@ XXtotal = scale(XXtotal);
 %    Disregard background (label=0)
 %----------------------------------------------------------------------
 
-rate = 0.05;       % Rate of selected pixels per class
+rate = 0.0001;       % Rate of selected pixels per class
 Xtrain = [];
 Ytrain = [];
 for class=1:NumClases		% skip class 0
@@ -67,115 +67,161 @@ clc
 nozero = find(YYtotal~=0);
 
 % Methods to be compared
-% METHODS = {'LDA' 'QDA' 'MAHALANOBIS' 'k-NN' 'TREE' 'BAG' 'ADABOOST' 'RF' 'NN' 'SVM' 'GPC'}
-METHODS = {'LDA' 'SVM' 'GPC'}
+METHODS = {'LDA' 'QDA' 'MAHAL' 'KNN' 'TREE' 'BAG' 'BOOST' 'RF' 'NN' 'SVM' 'GPC'}
+% METHODS = {'LDA', 'SVM', 'GPC'}
 
-% (1) LDA
-disp('Training LDA ...')
-t = cputime;
-Ypred_LDA  = classify(XXtotal,Xtrain,Ytrain);
-time_LDA = cputime-t;
-RES_LDA    = assessment(YYtotal(nozero),Ypred_LDA(nozero),'class');
-OAS_LDA    = RES_LDA.OA;
-KAPPA_LDA = RES_LDA.Kappa
+MM=0;
 
-% % (2) QDA
-% disp('Training QDA ...')
-% t = cputime;
-% Ypred_QDA  = classify(XXtotal,Xtrain,Ytrain,'quadratic');
-% time_QDA= cputime-t;
-% RES_QDA    = assessment(YYtotal(nozero),Ypred_QDA(nozero),'class');
-% OAS_QDA    = RES_QDA.OA;
-% KAPPA_QDA = RES_QDA.Kappa
-% 
-% % (3) MAHALANOBIS
-% disp('Training Mahalanobis LDA ...')
-% t = cputime;
-% Ypred_MAHALANOBIS  = classify(XXtotal,Xtrain,Ytrain,'mahalanobis');
-% time_MAHAL = cputime-t;
-% RES_MAHALANOBIS    = assessment(YYtotal(nozero),Ypred_MAHALANOBIS(nozero),'class');
-% OAS_MAHALANOBIS    = RES_MAHALANOBIS.OA;
-% KAPPA_MAHALANOBIS = RES_MAHALANOBIS.Kappa
-% 
-% % (4) k-NN
-% disp('Training k-NN ...')
-% t = cputime;
-% Ypred_KNN  = knnclassify(XXtotal,Xtrain,Ytrain);
-% time_KNN = cputime-t;
-% RES_KNN    = assessment(YYtotal(nozero),Ypred_KNN(nozero),'class');
-% OAS_KNN    = RES_KNN.OA;
-% KAPPA_KNN = RES_KNN.Kappa
-% 
-% % (5) TREES
-% fprintf('Training Trees ... \n')
-% t = cputime;
-% model_TREE = treefit(Xtrain,Ytrain,'method','classification');
-% time_TREE = cputime-t;
-% Ypred_TREE = treeval(model_TREE,XXtotal);
-% RES_TREE    = assessment(YYtotal(nozero),Ypred_TREE(nozero),'class');
-% OAS_TREE    = RES_TREE.OA;
-% KAPPA_TREE = RES_TREE.Kappa
-% 
-% % (6) Bagging Trees
-% fprintf('Training Bagging Trees ... \n')
-% ntrees = 200; % Number of trees in the bag
-% t = cputime;
-% bag = fitensemble(Xtrain,Ytrain,'Bag',ntrees,'Tree','type','classification');
-% time_BAG = cputime-t;
-% Ypred_BAG = predict(bag,XXtotal);
-% RES_BAG    = assessment(YYtotal(nozero),Ypred_BAG(nozero),'class');
-% OAS_BAG    = RES_BAG.OA;
-% KAPPA_BAG = RES_BAG.Kappa
-% 
-% % (7) AdaBoostM2 trees
-% fprintf('Training Boosting Trees ... \n')
-% ntrees = 200; % Number of trees for boosting
-% t = cputime;
-% boost = fitensemble(Xtrain,Ytrain,'AdaBoostM2',ntrees,'Tree','type','classification');
-% time_BOOST = cputime-t;
-% Ypred_BOOST = predict(boost,XXtotal);
-% RES_BOOST    = assessment(YYtotal(nozero),Ypred_BOOST(nozero),'class');
-% OAS_BOOST    = RES_BOOST.OA;
-% KAPPA_BOOST = RES_BOOST.Kappa
-% 
-% % (8) Random forest
-% ntrees = 200; % Number of trees for the forest
-% t = cputime;
-% forest = TreeBagger(ntrees,Xtrain,Ytrain);
-% time_RF = cputime-t;
-% Ypred_RF = predict(forest,XXtotal);
-% Ypred_RF = str2double(Ypred_RF);
-% RES_RF    = assessment(YYtotal(nozero),Ypred_RF(nozero),'class');
-% OAS_RF    = RES_RF.OA;
-% KAPPA_RF = RES_RF.Kappa
-% 
-% % (9) Neural networks
-% disp('Training NNs ...')
-% t = cputime;
-% model = trainNN(Xtrain,Ytrain);
-% time_NN = cputime-t;
-% Ypred_NN = testNN(model,XXtotal);
-% RES_NN    = assessment(YYtotal(nozero), Ypred_NN(nozero),'class');
-% OAS_NN    = RES_NN.OA;
-% KAPPA_NN = RES_NN.Kappa
+if sum(strcmpi(METHODS,'LDA'))
+    % (1) LDA
+    MM=MM+1;
+    disp('Training LDA ...')
+    t = cputime;
+    Ypred  = classify(XXtotal,Xtrain,Ytrain);
+    Yp(:,MM)=Ypred;
+    time(MM)   = cputime-t;
+    RES    = assessment(YYtotal(nozero),Ypred(nozero),'class');
+    OAS(MM)    = RES.OA;
+    KAPPA(MM)  = RES.Kappa;
+end
 
-% (10) Support vector machines (SVM)
-disp('Training SVM ...')
-t = cputime;
-Ypred_SVM = classifySVM(XXtotal,Xtrain,Ytrain);
-time_SVM = cputime-t;
-RES_SVM = assessment(YYtotal(nozero),Ypred_SVM(nozero),'class');
-OAS_SVM    = RES_SVM.OA;
-KAPPA_SVM = RES_SVM.Kappa
+if sum(strcmpi(METHODS,'QDA'))
+    % (2) QDA
+    MM=MM+1;
+    disp('Training QDA ...')
+    t = cputime;
+    Ypred  = classify(XXtotal,Xtrain,Ytrain,'quadratic');
+    Yp(:,MM)=Ypred;
+    time(MM) = cputime-t;
+    RES    = assessment(YYtotal(nozero),Ypred(nozero),'class');
+    OAS(MM)    = RES.OA;
+    KAPPA(MM) = RES.Kappa;
+end
 
-% (11) Gaussian Process Classification (GPC)
-disp('Training GPC ...')
-t = cputime;
-[Ypred_GPC PROBS MUS S2] = classifyGPC(XXtotal,Xtrain,Ytrain);
-time_GPC = cputime-t;
-RES_GPC = assessment(YYtotal(nozero),Ypred_GPC(nozero),'class');
-OAS_GPC    = RES_GPC.OA;
-KAPPA_GPC = RES_GPC.Kappa
+if sum(strcmpi(METHODS,'MAHAL'))
+    % (3) MAHALANOBIS
+    MM=MM+1;
+    disp('Training Mahalanobis LDA ...')
+    t = cputime;
+    Ypred  = classify(XXtotal,Xtrain,Ytrain,'mahalanobis');
+    Yp(:,MM)=Ypred;
+    time(MM) = cputime-t;
+    RES    = assessment(YYtotal(nozero),Ypred(nozero),'class');
+    OAS(MM)    = RES.OA;
+    KAPPA(MM) = RES.Kappa;
+end
+
+if sum(strcmpi(METHODS,'KNN'))
+    % (4) k-NN
+    MM=MM+1;
+    disp('Training k-NN ...')
+    t = cputime;
+    Ypred  = knnclassify(XXtotal,Xtrain,Ytrain);
+    Yp(:,MM)=Ypred;
+    time(MM) = cputime-t;
+    RES    = assessment(YYtotal(nozero),Ypred(nozero),'class');
+    OAS(MM)    = RES.OA;
+    KAPPA(MM) = RES.Kappa;
+end
+
+if sum(strcmpi(METHODS,'TREE'))
+    % (5) TREES
+    MM=MM+1;
+    fprintf('Training Trees ... \n')
+    t = cputime;
+    model = treefit(Xtrain,Ytrain,'method','classification');
+    time(MM) = cputime-t;
+    Ypred = treeval(model,XXtotal);
+    Yp(:,MM)=Ypred;
+    RES    = assessment(YYtotal(nozero),Ypred(nozero),'class');
+    OAS(MM)    = RES.OA;
+    KAPPA(MM) = RES.Kappa;
+end
+
+if sum(strcmpi(METHODS,'BAG'))
+    % (6) Bagging Trees
+    MM=MM+1;
+    fprintf('Training Bagging Trees ... \n')
+    ntrees = 200; % Number of trees in the bag
+    t = cputime;
+    bag = fitensemble(Xtrain,Ytrain,'Bag',ntrees,'Tree','type','classification');
+    time(MM) = cputime-t;
+    Ypred = predict(bag,XXtotal);
+    Yp(:,MM)=Ypred;
+    RES    = assessment(YYtotal(nozero),Ypred(nozero),'class');
+    OAS(MM)    = RES.OA;
+    KAPPA(MM) = RES.Kappa;
+end
+
+if sum(strcmpi(METHODS,'BOOST'))
+    % (7) AdaBoostM2 trees
+    MM=MM+1;
+    fprintf('Training Boosting Trees ... \n')
+    ntrees = 200; % Number of trees for boosting
+    t = cputime;
+    boost = fitensemble(Xtrain,Ytrain,'AdaBoostM2',ntrees,'Tree','type','classification');
+    time(MM) = cputime-t;
+    Ypred = predict(boost,XXtotal);
+    Yp(:,MM)=Ypred;
+    RES    = assessment(YYtotal(nozero),Ypred(nozero),'class');
+    OAS(MM)    = RES.OA;
+    KAPPA(MM) = RES.Kappa;
+end
+
+if sum(strcmpi(METHODS,'RF'))
+    % (8) Random forest
+    MM=MM+1;
+    ntrees = 200; % Number of trees for the forest
+    t = cputime;
+    forest = TreeBagger(ntrees,Xtrain,Ytrain);
+    time(MM) = cputime-t;
+    Ypred = predict(forest,XXtotal);
+    Ypred = str2double(Ypred);
+    Yp(:,MM)=Ypred;
+    RES    = assessment(YYtotal(nozero),Ypred(nozero),'class');
+    OAS(MM)    = RES.OA;
+    KAPPA(MM) = RES.Kappa;
+end
+
+if sum(strcmpi(METHODS,'NN'))
+    % (9) Neural networks
+    MM=MM+1;
+    disp('Training NNs ...')
+    t = cputime;
+    model = trainNN(Xtrain,Ytrain);
+    time(MM) = cputime-t;
+    Ypred = testNN(model,XXtotal);
+    Yp(:,MM)=Ypred;
+    RES    = assessment(YYtotal(nozero), Ypred(nozero),'class');
+    OAS(MM)    = RES.OA;
+    KAPPA(MM) = RES.Kappa;
+end
+
+if sum(strcmpi(METHODS,'SVM'))
+    % (10) Support vector machines (SVM)
+    MM=MM+1;
+    disp('Training SVM ...')
+    t = cputime;
+    Ypred = classifySVM(XXtotal,Xtrain,Ytrain);
+    Yp(:,MM)=Ypred;
+    time(MM) = cputime-t;
+    RES = assessment(YYtotal(nozero),Ypred(nozero),'class');
+    OAS(MM)    = RES.OA;
+    KAPPA(MM) = RES.Kappa;
+end
+
+if sum(strcmpi(METHODS,'GPC'))
+    % (11) Gaussian Process Classification (GPC)
+    MM=MM+1;
+    disp('Training GPC ...')
+    t = cputime;
+    [Ypred PROBS MUS S2] = classifyGPC(XXtotal,Xtrain,Ytrain);
+    time(MM) = cputime-t;
+    Yp(:,MM)=Ypred;
+    RES = assessment(YYtotal(nozero),Ypred(nozero),'class');
+    OAS(MM)    = RES.OA;
+    KAPPA(MM) = RES.Kappa;
+end
 
 %----------------------------------------------------------------------
 % 5) Results
@@ -184,7 +230,7 @@ KAPPA_GPC = RES_GPC.Kappa
 % Accuracy
 
 figure,
-bar([OAS_LDA,OAS_QDA,OAS_MAHALANOBIS,OAS_KNN,OAS_TREE,OAS_BAG,OAS_BOOST,OAS_RF,OAS_NN,OAS_SVM,OAS_GPC])
+bar(OAS)
 set(gca,'Xtick',1:11,'XTickLabel',METHODS)
 ylabel('Overall accuracy, OA[%]')
 grid
@@ -192,14 +238,14 @@ grid
 % Kappa statistic
 
 figure,
-bar([KAPPA_LDA,KAPPA_QDA,KAPPA_MAHALANOBIS,KAPPA_KNN,KAPPA_TREE,KAPPA_BAG,KAPPA_BOOST,KAPPA_RF,KAPPA_NN,KAPPA_SVM,KAPPA_GPC])
+bar(KAPPA)
 set(gca,'Xtick',1:11,'XTickLabel',METHODS)
 ylabel('\kappa statistic')
 grid
 
 % CPU times
 figure,
-barh(log10([time_LDA,time_QDA,time_MAHAL,time_KNN,time_TREE,time_BAG,time_BOOST,time_RF,time_NN,time_SVM,time_GPC]))
+barh(log10(time))
 xlabel('')
 set(gca,'YTickLabel',METHODS);
 xlabel('CPU Time log([s])')
@@ -214,18 +260,14 @@ RGB = reshape(I/max(I(:)),[r,c,3]);
 figure,imagesc(RGB), axis off square, title('Ground truth')
 % Ground truth
 figure,imagesc(reshape(YYtotal,r,c)), axis off square, title('Ground truth')
-% Classification maps
-figure,imagesc(reshape(Ypred_LDA,r,c)), axis off square, title(['LDA, Kappa=' num2str(KAPPA_LDA)])
-figure,imagesc(reshape(Ypred_QDA,r,c)), axis off square, title(['QDA, Kappa=' num2str(KAPPA_QDA)])
-figure,imagesc(reshape(Ypred_MAHALANOBIS,r,c)), axis off square, title(['MAHALANOBIS, Kappa=' num2str(KAPPA_MAHALANOBIS)])
-figure,imagesc(reshape(Ypred_KNN,r,c)), axis off square, title(['k-NN, Kappa=' num2str(KAPPA_KNN)])
-figure,imagesc(reshape(Ypred_TREE,r,c)), axis off square, title(['TREE, Kappa=' num2str(KAPPA_TREE)])
-figure,imagesc(reshape(Ypred_BAG,r,c)), axis off square, title(['BAG, Kappa=' num2str(KAPPA_BAG)])
-figure,imagesc(reshape(Ypred_BOOST,r,c)), axis off square, title(['BOOST, Kappa=' num2str(KAPPA_BOOST)])
-figure,imagesc(reshape(Ypred_RF,r,c)), axis off square, title(['RF, Kappa=' num2str(KAPPA_RF)])
-figure,imagesc(reshape(Ypred_NN,r,c)), axis off square, title(['NN, Kappa=' num2str(KAPPA_NN)])
-figure,imagesc(reshape(Ypred_SVM,r,c)), axis off square, title(['SVM, Kappa=' num2str(KAPPA_SVM)])
-figure,imagesc(reshape(Ypred_GPC,r,c)), axis off square, title(['SVM, Kappa=' num2str(KAPPA_GPC)])
 
-% % Sort figures in the screen
+% Classification maps
+for mm=1:MM
+    figure,imagesc(reshape(Yp(:,mm),r,c)), 
+    axis off square, title([METHODS{mm} ', Kappa=' num2str(KAPPA(mm))])
+end
+
+% Sort figures in the screen
 tile
+
+
